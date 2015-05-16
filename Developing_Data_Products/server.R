98830a2131411f9b9ed2dbb93ed18eaa13a4f823
John Akwei <johnakwei1@gmail.com>
2015-05-16 09:11:41 -0400

    uploading

--- Developing_Data_Products/server.R
+++ Developing_Data_Products/server.R
@@ -7,8 +7,7 @@ CountyState <- dt[3:4]
 CountyState[] <- lapply(CountyState, as.character)
 RiskFactors <- cbind(dt[7], dt[10], dt[13], dt[16], dt[19], dt[22])
 CountyState <- cbind(CountyState, RiskFactors)
-rm(dt)
-rm(RiskFactors)
+rm(dt, RiskFactors)
 dt2 <- read.csv("chsi_dataset/DEMOGRAPHICS.csv")
 dt2Columns <- cbind(dt2[12], dt2[15], dt2[30], dt2[33], dt2[36], dt2[39], dt2[42])
 rm(dt2)
@@ -71,45 +70,22 @@ shinyServer(
     
     # Calculate and Graph Average State Risk Factors.
     output$StateHist <- renderPlot({
-            StateAvg <- subset(na.omit(CountyState),
+            StateAvg <- subset(CountyState,
             CountyState$CHSI_State_Name==input$State)
-            NoExercise <- mean(StateAvg$No_Exercise)
-            NoExer <- as.data.frame(NoExercise)
-            FewFruitVeg <- mean(StateAvg$Few_Fruit_Veg)
-            FewFruit <- as.data.frame(FewFruitVeg)
-            Obesity <- mean(StateAvg$Obesity)
-            Obes <- as.data.frame(Obesity)
-            HighBloodPressure <- mean(StateAvg$High_Blood_Pres)
-            HighBlood <- as.data.frame(HighBloodPressure)
-            Smoker <- mean(StateAvg$Smoker)
-            Smoke <- as.data.frame(Smoker)
-            Diabetes <- mean(StateAvg$Diabetes)
-            Diab <- as.data.frame(Diabetes)
-            finalPlot <- cbind(NoExer, FewFruit, Obes,
-                               HighBlood, Smoke, Diab)
+            finalPlot <- lapply(na.omit(StateAvg[3:8]), mean)
+            finalPlot <- as.data.frame(finalPlot)
             barplot(as.matrix(finalPlot),
-            col="orange", main="State Average Health Risk Factors",
-            xlab="Average Risk Factors", ylab="Risk Factors Level")})
-    
+                  col="orange", main="State Average Health Risk Factors",
+                  xlab="Average Risk Factors", ylab="Risk Factors Level")
+            })
+
     # Calculate and Graph Average USA Risk Factors.
     output$USAHist <- renderPlot({
             USAvg <- na.omit(CountyState)
-            NoExercise <- mean(USAvg$No_Exercise)
-            NoExer <- as.data.frame(NoExercise)
-            FewFruitVeg <- mean(USAvg$Few_Fruit_Veg)
-            FewFruit <- as.data.frame(FewFruitVeg)
-            Obesity <- mean(USAvg$Obesity)
-            Obes <- as.data.frame(Obesity)
-            HighBloodPressure <- mean(USAvg$High_Blood_Pres)
-            HighBlood <- as.data.frame(HighBloodPressure)
-            Smoker <- mean(USAvg$Smoker)
-            Smoke <- as.data.frame(Smoker)
-            Diabetes <- mean(USAvg$Diabetes)
-            Diab <- as.data.frame(Diabetes)
-            finalUSPlot <- cbind(NoExer, FewFruit, Obes,
-                               HighBlood, Smoke, Diab)
+            finalUSPlot <- lapply(USAvg[3:8], mean)
+            finalUSPlot <- as.data.frame(finalUSPlot)
             barplot(as.matrix(finalUSPlot),
             col="orange", main="USA Average Health Risk Factors",
-            xlab="Average Risk Factors", ylab="Risk Factors Level")})
-    }
-  )
+            xlab="Average Risk Factors", ylab="Risk Factors Level")
+            })
+})
